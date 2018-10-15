f3e8e9245d6e7ce9892096e318d72604f697a081
aihara005 <asahi.aihara@leverages.jp>
2018-10-15 09:35:16 +0900

    数値入力処理、View作成まで

--- resources/assets/js/slot.js
+++ resources/assets/js/slot.js
@@ -1,7 +1,27 @@
 $(() => {
-    const
-        box = document.getElementsByClassName('box');
-    box.on('click', e => {
-        alert('clicked');
+    class slot {
+        constructor() {
+            this.time = 0;
+            this.setRandNum('box_1');
+            this.setRandNum('box_2');
+            this.setRandNum('box_3');
+        }
+
+        // 数字をランダムで入力する処理
+        setRandNum(id) {
+            setInterval(function() {
+                document.getElementById(id).value = Math.floor(Math.random() * 10);
+            }, 50);
+        }
+
+        // 数字を止める処理
+        stopSlotBox(id) {
+            clearInterval(id);
+        }
+    }
+    const Slot = new slot();
+    $('.item').on('click',  e => {
+        alert(e.target.id);
+        Slot.stopSlotBox(e.target.id);
     })
 });
