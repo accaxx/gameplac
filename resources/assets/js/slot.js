8e2e6727633f7ef02be64e93ae170898ae0b5446
aihara005 <asahi.aihara@leverages.jp>
2018-10-16 09:43:14 +0900

    Slot 動作するもの

--- resources/assets/js/slot.js
+++ resources/assets/js/slot.js
@@ -2,26 +2,53 @@ $(() => {
     class slot {
         constructor() {
             this.time = 0;
-            this.setRandNum('box_1');
-            this.setRandNum('box_2');
-            this.setRandNum('box_3');
+            this.setRandNum();
         }
 
         // 数字をランダムで入力する処理
-        setRandNum(id) {
-            setInterval(function() {
-                document.getElementById(id).value = Math.floor(Math.random() * 10);
-            }, 50);
+        setRandNum() {
+            for (let i = 1; i <= 3; i++) {
+                this['box_' + i] = setInterval(function() {
+                    document.getElementById('box_' + i).value = Math.floor(Math.random() * 10);
+                }, 50);
+            }
         }
 
         // 数字を止める処理
         stopSlotBox(id) {
-            clearInterval(id);
+            clearInterval(this[id]);
+            if (Slot.isFinishOrContinue()) {
+                Slot.getResultByBoxes();
+            }
+            return;
+        }
+
+        // 終わりか判定する処理
+        isFinishOrContinue() {
+            if (document.getElementsByClassName('fixed').length == 3) {
+                return true;
+            }
+            return false;
+        }
+
+        // 3つのboxから結果を取得する処理
+        getResultByBoxes() {
+            for (let i = 1; i <= 3; i++) {
+                this['result_' + i] = document.getElementById('box_' + i).value;
+            }
+
+            if (this['result_1'] == this['result_2'] && this['result_1'] == this['result_3']) {
+                alert('当たり!');
+                return;
+            }
+            alert('はずれ');
+            return;
         }
     }
+
     const Slot = new slot();
     $('.item').on('click',  e => {
-        alert(e.target.id);
+        e.currentTarget.classList.add('fixed');
         Slot.stopSlotBox(e.target.id);
     })
 });
